5a299b67635745143bfd89cf9a67c781de4444e2
Mazuki <mazuki.wu@gmail.com>
2025-07-18 14:33:47 -0500

    Add operator function

--- main.js
+++ main.js
@@ -2,6 +2,26 @@ let firstNum;
 let operator;
 let secondNum;
 
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum);
+            break;
+        case "-":
+            return subtract(firstNum, secondNum);
+            break;
+        case "*":
+            return multiply(firstNum, secondNum);
+            break;
+        case "/":
+            return divide(firstNum, secondNum);
+            break;
+        default:
+            return "Bad input";
+            break;
+    }
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
