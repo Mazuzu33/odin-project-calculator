d210508a9189ca50529d0002f895d80130d3d1fe
Mazuki <mazuki.wu@gmail.com>
2025-07-18 18:53:57 -0500

    Add button functionality
    Pressing numbers and operators now updates
    the display accordingly to represent
    the value of the expression.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let firstNum;
-let operator;
-let secondNum;
+let firstNum = null;
+let operator = null;
+let secondNum = null;
 
 function operate(operator, firstNum, secondNum) {
     switch (operator) {
@@ -37,4 +37,79 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
    return num1 / num2;
 }
+
+let display = document.querySelector(".display");
+let rows = document.querySelector(".rows");
+let validDigit = "0123456789";
+let validOperator = "+-x/";
+
+function displayLog(num) {
+    display.textContent = num;
+}
+
+function buttonClick(e) {
+    if (e.target.tagName === "BUTTON") {
+        buttonContent = e.target.textContent;
+        //Button contains a number
+        if (validDigit.includes(buttonContent)) {
+            //Set firstNum
+            if (firstNum === null) {
+                firstNum = Number(buttonContent);
+                displayLog(firstNum);
+            }
+            //Append new digit to firstNum
+            else if (operator === null) {
+                firstNum = firstNum + buttonContent;
+                firstNum = Number(firstNum);
+                displayLog(firstNum);
+            }
+            //Set secondNum
+            else {
+                secondNum = Number(buttonContent)
+                displayLog(secondNum);
+            }
+        }
+        //Button contains Clear
+        else if (buttonContent === "Clear") {
+            firstNum = null;
+            operator = null;
+            secondNum = null;
+            displayLog("");
+        }
+        //Button contains a operator other than =
+        else if (validOperator.includes(buttonContent)) {
+            operator = buttonContent;
+        }
+        //Button contains =
+        else {
+            if (firstNum !== null && operator != null && operator != null) {
+                switch (operator) {
+                    case "+":
+                        firstNum = add(firstNum, secondNum);
+                        break;
+                    case "-":
+                        firstNum = subtract(firstNum, secondNum);
+                        break;
+                    case "x":
+                        firstNum = multiply(firstNum, secondNum);
+                        break;
+                    case "/":
+                        firstNum = divide(firstNum, secondNum);
+                        break;
+                }
+                displayLog(firstNum);
+                operator = null;
+                secondNum = null;
+            }
+        }
+    }
+}
+
+
+rows.addEventListener("click", buttonClick);
+
+
+//Add event listeners for each button
+//When button is clicked, get its value through textContent
+//add textcontent to display text content
 
