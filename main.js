38bb68012382f43960bc67cd858105738cff9479
Mazuki <mazuki.wu@gmail.com>
2025-07-22 16:03:39 -0500

    Add keyboard functionality
    Pressing corresponging keys will activate buttons
    the same as if they were clicked on.

--- main.js
+++ main.js
@@ -55,124 +55,125 @@ function displayLog(num) {
     }
 }
 
-function buttonClick(e) {
-    if (e.target.tagName === "BUTTON") {
-        buttonContent = e.target.textContent;
-        //Button is a number
-        if (validDigit.includes(buttonContent)) {
-            //Set firstNum
-            if (firstNum === null || displayingResult) {
-                firstNum = Number(buttonContent);
-                displayLog(firstNum);
-                displayingResult = false;
-            }
-            //Append digit to firstNum
-            else if (operator === null) {
-                if (decimalPoint) {
-                    firstNum += ".";
-                    decimalPoint = false;
-                }
-                firstNum += buttonContent;
-                firstNum = Number(firstNum);
-                displayLog(firstNum);
-            }
-            //Set secondNum
-            else if (secondNum === null) {
-                secondNum = Number(buttonContent)
-                displayLog(secondNum);
-            }
-            //Append digit to secondNum
-            else {
-                if (decimalPoint) {
-                    secondNum += ".";
-                    decimalPoint = false;
-                }
-                secondNum += buttonContent;
-                secondNum = Number(secondNum);
-                displayLog(secondNum);
-            }
-        }
-        //Button is decimal point
-        else if (buttonContent === ".") {
-            //Append . to firstNum
-            if (firstNum !== null && operator === null && !String(firstNum).includes(".") && !displayingResult) {
-                decimalPoint = true;
-                display.textContent += ".";
-            }
-            //Append . to secondNum
-            else if (secondNum !== null && !String(secondNum).includes(".")) {
-                decimalPoint = true;
-                display.textContent += ".";
-            }
-
-        }
-        //Button is Clear
-        else if (buttonContent === "Clear") {
-            firstNum = null;
-            operator = null;
-            secondNum = null;
+function buttonUse(e) {
+    let content = e.key ? e.key : e.target.textContent;
+    console.log(Boolean(e.key));
+    console.log(validDigit.includes(e.key));
+    //Button is a number
+    if (validDigit.includes(content)) {
+        //Set firstNum
+        if (firstNum === null || displayingResult) {
+            firstNum = Number(content);
+            displayLog(firstNum);
             displayingResult = false;
-            displayLog("");
         }
-        //Button is delete
-        else if (buttonContent === "Delete") {
-            console.log("test");
-            if (secondNum !== null) {
-                if (decimalPoint) {
-                        decimalPoint = false;
-                        display.textContent = display.textContent.slice(0, -1);
-                    }
-                else if (String(secondNum).length === 1) {
-                    secondNum = null;
-                    displayLog("");
-                }
-                else {
-                    secondNum = Number(String(secondNum).slice(0, -1));
-                    displayLog(secondNum);
-                }
+        //Append digit to firstNum
+        else if (operator === null) {
+            if (decimalPoint) {
+                firstNum += ".";
+                decimalPoint = false;
             }
-            else if (firstNum !== null && operator === null) {
-                if (decimalPoint) {
+            firstNum += content;
+            firstNum = Number(firstNum);
+            displayLog(firstNum);
+        }
+        //Set secondNum
+        else if (secondNum === null) {
+            secondNum = Number(content)
+            displayLog(secondNum);
+        }
+        //Append digit to secondNum
+        else {
+            if (decimalPoint) {
+                secondNum += ".";
+                decimalPoint = false;
+            }
+            secondNum += content;
+            secondNum = Number(secondNum);
+            displayLog(secondNum);
+        }
+    }
+    //Button is decimal point
+    else if (content === ".") {
+        //Append . to firstNum
+        if (firstNum !== null && operator === null && !String(firstNum).includes(".") && !displayingResult) {
+            decimalPoint = true;
+            display.textContent += ".";
+        }
+        //Append . to secondNum
+        else if (secondNum !== null && !String(secondNum).includes(".")) {
+            decimalPoint = true;
+            display.textContent += ".";
+        }
+
+    }
+    //Button is Clear
+    else if (content === "Clear") {
+        firstNum = null;
+        operator = null;
+        secondNum = null;
+        displayingResult = false;
+        displayLog("");
+    }
+    //Button is delete
+    else if (content === "Delete") {
+        console.log("test");
+        if (secondNum !== null) {
+            if (decimalPoint) {
                     decimalPoint = false;
                     display.textContent = display.textContent.slice(0, -1);
                 }
-                else if (String(firstNum).length === 1) {
-                    firstNum = null;
-                    displayLog("");
-                }
-                else {
-                    firstNum = Number(String(firstNum).slice(0, -1));
-                    displayLog(firstNum);
-                }
+            else if (String(secondNum).length === 1) {
+                secondNum = null;
+                displayLog("");
+            }
+            else {
+                secondNum = Number(String(secondNum).slice(0, -1));
+                displayLog(secondNum);
             }
         }
-        //Button is a operator other than =
-        else if (validOperator.includes(buttonContent)) {
-            //Operator is being added onto an already complete expression
-            if (secondNum !== null) {
-                firstNum = operate(operator, firstNum, secondNum);
-                displayLog(firstNum);
-                operator = buttonContent;
-                secondNum = null;
+        else if (firstNum !== null && operator === null) {
+            if (decimalPoint) {
+                decimalPoint = false;
+                display.textContent = display.textContent.slice(0, -1);
             }
-            //Operator is being chosen or changed
-            else if (firstNum !== null) {
-                operator = buttonContent;
-                displayingResult = false;
+            else if (String(firstNum).length === 1) {
+                firstNum = null;
+                displayLog("");
+            }
+            else {
+                firstNum = Number(String(firstNum).slice(0, -1));
+                displayLog(firstNum);
             }
         }
-        //Button is =
-        else {
-            if (firstNum !== null && operator != null && secondNum != null) {
-                firstNum = operate(operator, firstNum, secondNum);
-                displayLog(firstNum);
-                operator = null;
-                secondNum = null;
-                displayingResult = true;
-            }
+    }
+    //Button is a operator other than =
+    else if (validOperator.includes(content)) {
+        //Operator is being added onto an already complete expression
+        if (secondNum !== null) {
+            firstNum = operate(operator, firstNum, secondNum);
+            displayLog(firstNum);
+            operator = content;
+            secondNum = null;
+        }
+        //Operator is being chosen or changed
+        else if (firstNum !== null) {
+            operator = content;
+            displayingResult = false;
+        }
+    }
+    //Button is =
+    else if (content == "=") {
+        if (firstNum !== null && operator != null && secondNum != null) {
+            firstNum = operate(operator, firstNum, secondNum);
+            displayLog(firstNum);
+            operator = null;
+            secondNum = null;
+            displayingResult = true;
         }
     }
 }
 
 
-rows.addEventListener("click", buttonClick);
+rows.addEventListener("click", buttonUse);
+window.addEventListener("keydown", buttonUse);
